48e3260f121fcd6c0d0636b13e918d23156fe753
Ystziso <justin8803989@gmail.com>
2023-04-21 13:19:52 +0800

    第一次存檔

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./index.scss";
+// import "bootstrap/js/dist/collapse";
+// import "bootstrap/js/dist/alert";
+// import "bootstrap/js/dist/button";
+// import "bootstrap/js/dist/carousel";
+import * as bootstrap from "bootstrap";
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin()],
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$|\.scss$/i,
+        use: [
+          {
+            // inject CSS to page
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            // translates CSS into CommonJS modules
+            loader: "css-loader",
+          },
+          {
+            // Run postcss actions
+            loader: "postcss-loader",
+            options: {
+              // `postcssOptions` is needed for postcss 8.x;
+              // if you use postcss 7.x skip the key
+              postcssOptions: {
+                // postcss plugins, can be exported to postcss.config.js
+                plugins: function () {
+                  return [require("autoprefixer")];
+                },
+              },
+            },
+          },
+          {
+            // compiles Sass to CSS
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
